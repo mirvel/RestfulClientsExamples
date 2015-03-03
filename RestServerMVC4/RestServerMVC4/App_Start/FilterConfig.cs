95f4ebfdf4dd13630ddda2b19e5295c3c39ddfbc
Mirlan Davletov <mirlandavletov@rideau.com>
2015-03-03 08:47:50 -0500

    RestServerMVC4
    New web api project based on MVC4 is added to folder

---
+++ RestServerMVC4/RestServerMVC4/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RestServerMVC4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ RestServerMVC4/RestServerMVC4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestServerMVC4.WebApiApplication" Language="C#" %>
